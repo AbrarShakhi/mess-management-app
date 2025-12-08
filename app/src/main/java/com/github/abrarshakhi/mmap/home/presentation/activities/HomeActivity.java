f8dfd9872dca8ec5f7ba1bec662cadfe41003351
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2025-12-08 23:54:07 +0600

    fragments navigations

--- app/src/main/java/com/github/abrarshakhi/mmap/home/presentation/activities/HomeActivity.java
+++ app/src/main/java/com/github/abrarshakhi/mmap/home/presentation/activities/HomeActivity.java
@@ -7,20 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.github.abrarshakhi.mmap.R;
 import com.github.abrarshakhi.mmap.databinding.ActivityHomeBinding;
-import com.github.abrarshakhi.mmap.home.presentation.fragments.GroceryFragment;
-import com.github.abrarshakhi.mmap.home.presentation.fragments.HomeFragment;
-import com.github.abrarshakhi.mmap.home.presentation.fragments.ManagementFragment;
-import com.github.abrarshakhi.mmap.home.presentation.fragments.MealsFragment;
-import com.github.abrarshakhi.mmap.home.presentation.fragments.ProfileFragment;
+import com.github.abrarshakhi.mmap.home.presentation.navigations.NavDestination;
+import com.github.abrarshakhi.mmap.home.presentation.navigations.NavigationManager;
 
 public class HomeActivity extends AppCompatActivity {
-    ActivityHomeBinding binding;
+    private ActivityHomeBinding binding;
+
+    private NavigationManager navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,32 +30,19 @@ public class HomeActivity extends AppCompatActivity {
             return insets;
         });
 
+        navigation = new NavigationManager(
+            getSupportFragmentManager(),
+            R.id.flMainFrameLayout,
+            getSupportActionBar()
+        );
+
         setSupportActionBar(binding.topAppBar);
 
         binding.bnvMainNavBar.setOnItemSelectedListener(item -> {
-            if (item.getItemId() == R.id.bniHome) {
-                replaceFragment(new HomeFragment(), "Home");
-            } else if (item.getItemId() == R.id.bniMeals) {
-                replaceFragment(new MealsFragment(), "Meals Tracking");
-            } else if (item.getItemId() == R.id.bniGrocery) {
-                replaceFragment(new GroceryFragment(), "Grocery List");
-            } else if (item.getItemId() == R.id.bniManagement) {
-                replaceFragment(new ManagementFragment(), "Mess Management");
-            } else if (item.getItemId() == R.id.bniProfile) {
-                replaceFragment(new ProfileFragment(), "Profile");
-            }
+            navigation.navigate(NavDestination.fromMenuId(item.getItemId()));
             return true;
         });
-        replaceFragment(new HomeFragment(), "Home");
-    }
 
-    private void replaceFragment(Fragment fragment, String title) {
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle(title);
-        }
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.flMainFrameLayout, fragment);
-        fragmentTransaction.commit();
+        navigation.navigate(NavDestination.HOME);
     }
 }
